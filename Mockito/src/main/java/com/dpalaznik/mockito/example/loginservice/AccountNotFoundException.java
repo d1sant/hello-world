9a9472c56458629cab3b768a8c231bcf15f32142
Dmitry Palaznik <dpalaznik@gmail.com>
2013-04-14 02:02:49 +0300

    Mockito module - sixth test: compilation error fixed

---
+++ Mockito/src/main/java/com/dpalaznik/mockito/example/loginservice/AccountNotFoundException.java
@@ -0,0 +1,10 @@
+package com.dpalaznik.mockito.example.loginservice;
+
+/**
+ * Date: 2013-04-14
+ *
+ * @author Dmitry Palaznik <dmitry.palaznik@viaden.com>
+ */
+public class AccountNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
