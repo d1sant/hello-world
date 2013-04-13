45faeb3142a477bfab512fda29863784f1aa23ec
Dmitry Palaznik <dpalaznik@gmail.com>
2013-04-14 01:01:40 +0300

    Mockito module - refactoring

--- Mockito/src/test/java/com/dpalaznik/mockito/example/loginservice/LoginServiceTest.java
+++ Mockito/src/test/java/com/dpalaznik/mockito/example/loginservice/LoginServiceTest.java
@@ -1,5 +1,6 @@
 package com.dpalaznik.mockito.example.loginservice;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Matchers.anyString;
@@ -12,34 +13,35 @@ import static org.mockito.Mockito.*;
  */
 public class LoginServiceTest {
 
+    private IAccount account;
+    private IAccountRepository accountRepository;
+    private LoginService service;
+
+    @Before
+    public void init() {
+        account = mock(IAccount.class);
+        accountRepository = mock(IAccountRepository.class);
+        when(accountRepository.find(anyString())).thenReturn(account);
+        service = new LoginService(accountRepository);
+    }
+
     @Test
     public void isShouldSetAccountToLoggedInWhenPasswordMatches() {
-        IAccount account = mock(IAccount.class);
-        when(account.passwordMatches(anyString())).thenReturn(true);
-
-        IAccountRepository accountRepository = mock(IAccountRepository.class);
-        when(accountRepository.find(anyString())).thenReturn(account);
-
-        LoginService service = new LoginService(accountRepository);
+        willPasswordMatch(true);
         service.login("dmitry", "password");
-
         verify(account, times(1)).setLoggedIn(true);
     }
 
     @Test
     public void itShouldSetAccountToRevokedAfterThreeFailedLoginAttempts() {
-        IAccount account = mock(IAccount.class);
-        when(account.passwordMatches(anyString())).thenReturn(false);
-
-        IAccountRepository accountRepository = mock(IAccountRepository.class);
-        when(accountRepository.find(anyString())).thenReturn(account);
-
-        LoginService service = new LoginService(accountRepository);
-
+        willPasswordMatch(false);
         for (int i = 0; i < 3; i++) {
             service.login("dmitry", "password");
         }
-
         verify(account, times(1)).setRevoked(true);
+    }
+
+    private void willPasswordMatch(boolean value) {
+        when(account.passwordMatches(anyString())).thenReturn(value);
     }
 }
