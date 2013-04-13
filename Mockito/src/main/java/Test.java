5397dff1f7489762f1d01f8949836cc9508e956b
Dmitry Palaznik <dpalaznik@gmail.com>
2013-04-13 23:58:21 +0300

    Mockito module - initial version

---
+++ Mockito/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Date: 2013-04-13
+ *
+ * @author Dmitry Palaznik <dmitry.palaznik@viaden.com>
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Mockito");
+    }
+}
